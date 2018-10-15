9192cc382d481adc2ceb8297fa6745d5edb9a890
Xeno <rblxnub@gmail.com>
2018-10-15 14:54:32 +0200

    Finished opdracht1, started opdracht2.

---
+++ opdracht1.js
@@ -0,0 +1,6 @@
+function halloWereld(getal){ 
+	for (var i = 0; i <=getal; i++) {
+	document.write(i+". Hallo Wereld!<br>");
+	}
+}
+halloWereld(999)
---
+++ opdracht2.js
@@ -0,0 +1,6 @@
+var number1 = prompt('Noem nummer')
+var berekening = prompt('Noem somsoort')
+var number2 = prompt('Noem nummer')
+
+function berekenen(waarde1, waarde2)
+
